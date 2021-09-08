d72b9767aa7b9916be3f833c5ca5b44fbda97108
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-09-08 10:50:14 -0300

    Iniciando função e documentação

--- zenGroupModules/relatorio_03.py
+++ zenGroupModules/relatorio_03.py
@@ -1,1 +1,13 @@
 # Preparar um relatório que retorne os alunos que passaram direto sem a necessidade da nota do global solution
+
+def relatorio_alunos_passaram_sgs(media_checkpoints, nota_challenge):
+    '''
+        Essa função deverá retornar um dicionário com os alunos que passaram sem a nota do Global Solution
+        Formato do dicionario -> 
+        alunos_aprovados = {
+            "RM": "nome"
+        }
+
+        param1: media dos checkpoints
+        param2: nota do challenge
+    '''
