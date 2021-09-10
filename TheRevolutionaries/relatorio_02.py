1f853b7ecc835a2149c0b0442987d10a2dd456fc
thiago <rm87150@fiap.com.br>
2021-09-10 15:08:30 -0300

    relatório que retorne os alunos que não possuem chance de aprovação

--- TheRevolutionaries/relatorio_02.py
+++ TheRevolutionaries/relatorio_02.py
@@ -1,50 +1,33 @@
 #Preparar um relatório que retorne os alunos que não possuem chance de aprovação
 
-def relatorio_alunos_nao_passaram(media_primeiro_semestre, cp_01, cp_02, cp_03, nota_challenge_sprint3, nota_challenge_sprint4, nota_global_solution):
-    
+def relatorio_alunos_nao_passaram(alunos_data):
     '''
-        Essa função deverá retornar um dicionário com os alunos que não possuem chance de aprovação (não passarão mesmo que tirem 10 no Global Solution)
+        Essa função deverá retornar um dicionário com os alunos que não passarão mesmo tirando nota máxima no Global Solution
         Formato do dicionario -> 
-        alunos_nao_aprovados = {
+        alunos_não_aprovados = {
             "RM": "nome"
+            String: String
         }
-        param1: média do 1º semestre
-        param2: nota do Checkpoint 1
-        param3: nota do Checkpoint 2
-        param4: nota do Checkpoint 3
-        param5: nota do challenge sprint 3
-        param6: nota do challenge sprint 4
-        param7: nota do Global Solution
-
+        param1: Lista com os dados de todos os alunos
         Regra: A menor nota dos checkpoints é descartada
     '''
+    alunos_nao_aprovados_com_gs = dict()
 
-    cp_notas = [cp_01, cp_02, cp_03]
-    peso_primeiro_semestre = 0.4
-    peso_segundo_semestre = 0.6
-    menor_nota_cp = min(cp_notas) # Pegando a menor nota
-    cp_notas.remove(menor_nota_cp)
+    for i in alunos_data:
+        cp_notas = [float(i["semestre_02"]["checkpoint_01"]), float(i["semestre_02"]["checkpoint_02"]), float(i["semestre_02"]["checkpoint_01"])]
+        peso_primeiro_semestre = 0.4
+        peso_segundo_semestre = 0.6
+        media_gs = 10
+        menor_nota_cp = min(cp_notas) # Pegando a menor nota
+        cp_notas.remove(menor_nota_cp)
+        media_cp = sum(cp_notas) / 2
+        media_challenge = (float(i["semestre_02"]["challenge_03"]) + float(i["semestre_02"]["challenge_04"])) / 2
 
-    media_cp = sum(cp_notas) / 2
+        media_final = float(i["semestre_01"]) * peso_primeiro_semestre + (((((media_challenge + media_cp) / 2) * 0.4) + (media_gs * 0.6)) * peso_segundo_semestre)
 
-    media_challenge = (nota_challenge_sprint3 + nota_challenge_sprint4) / 2
-    
-    media_cp_challenge = (media_cp + media_challenge)/2
-
-    media_segundo_semestre = (media_cp_challenge * 0.4) + (nota_global_solution * 0.6)
-
-    media_final = (media_primeiro_semestre * peso_primeiro_semestre) + (media_segundo_semestre * peso_segundo_semestre)
-
-    # Teste
-    return media_final
+        # Condição para salvar o aluno na lista
+        if media_final <= 3.9:
+            alunos_nao_aprovados_com_gs[i["rm"]] = i["nome"]
 
 
-
-
-#Teste
-def teste():
-    alunos = relatorio_alunos_nao_passaram(0.5, 0, 0, 0, 0, 0, 10)
-    print(alunos)
-
-
-teste()
+    return alunos_nao_aprovados_com_gs
