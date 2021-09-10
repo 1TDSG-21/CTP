e2b7d9c88b177552021a34b267c32f9d82d62353
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-09-10 12:15:45 -0300

    Finalização do módulo

--- zenGroupModules/relatorio_03.py
+++ zenGroupModules/relatorio_03.py
@@ -1,6 +1,6 @@
 # Preparar um relatório que retorne os alunos que passaram direto sem a necessidade da nota do global solution
 
-def relatorio_alunos_passaram_sgs(media_primeiro_semestre, cp_01, cp_02, cp_03, nota_challenge_sprint3, nota_challenge_sprint4):
+def relatorio_alunos_passaram_sgs(alunos_data):
     '''
         Essa função deverá retornar um dicionário com os alunos que passaram sem a nota do Global Solution
         Formato do dicionario -> 
@@ -8,35 +8,34 @@ def relatorio_alunos_passaram_sgs(media_primeiro_semestre, cp_01, cp_02, cp_03, 
             "RM": "nome"
         }
 
-        param1: média do 1º semestre
-        param2: nota do Checkpoint 1
-        param3: nota do Checkpoint 2
-        param4: nota do Checkpoint 3
-        param5: nota do challenge sprint 3
-        param6: nota do challenge sprint 4
+        param1: Lista com os dados de todos os alunos
 
         Regra: A menor nota dos checkpoints é descartada
     '''
-    cp_notas = [cp_01, cp_02, cp_03]
-    peso_primeiro_semestre = 0.4
-    peso_segundo_semestre = 0.6
-    menor_nota_cp = min(cp_notas) # Pegando a menor nota
-    cp_notas.remove(menor_nota_cp)
+    alunos_aprovados_sem_gs = dict()
 
-    media_cp = sum(cp_notas) / 2
-    media_challenge = (nota_challenge_sprint3 + nota_challenge_sprint4) / 2
+    for i in alunos_data:
+        cp_notas = [i["checkpoint_01"], i["checkpoint_02"], i["checkpoint_01"]]
+        peso_primeiro_semestre = 0.4
+        peso_segundo_semestre = 0.6
+        menor_nota_cp = min(cp_notas) # Pegando a menor nota
+        cp_notas.remove(menor_nota_cp)
+        media_cp = sum(cp_notas) / 2
+        media_challenge = (i["challenge_03"] + i["challenge_04"]) / 2
+
+        media_final = i["semestre_01"] * peso_primeiro_semestre + (((media_challenge + media_cp) / 2) * 0.4) * peso_segundo_semestre
+
+        # Condição para salvar o aluno na lista
+        if media_final >= 7.0:
+            alunos_aprovados_sem_gs[i["rm"]] = i["nome"]
 
 
-    media_final = media_primeiro_semestre * peso_primeiro_semestre + (((media_challenge + media_cp) / 2) * 0.4) * peso_segundo_semestre
-
-    # Teste
-    return media_final
+    return alunos_aprovados_sem_gs
 
 
 # Teste
 def teste():
-    alunos = relatorio_alunos_passaram_sgs(10, 5, 10, 8, 8, 7)
-    print(alunos)
+    ...
 
 
 teste()
